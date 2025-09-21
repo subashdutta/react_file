0ba00e97f801b1be6271e911bec1e65079ae843c
shubashdutta <141384565+shubashdutta@users.noreply.github.com>
2025-09-21 13:26:28 +0545

    git depoly

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/react_file/",
   server: {
     port: 3000,
   },
